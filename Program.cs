b0e904f20485e1c589fb95ee6f8e502c34855e23
MasyoLab <masyo.lab@gmail.com>
2023-07-30 23:43:00 +0900

    クラスを分けただけ

--- Program.cs
+++ Program.cs
@@ -3,67 +3,110 @@ using System.Linq;
 using System.IO;
 using System.Collections.Generic;
 
-namespace UserIDCheck {
+namespace UserIDCheck
+{
+    struct AppConstData
+    {
+        public const int FIRST_INDEX = 1;
+        public const string STR_BEGIN = "(ID[";
+        public const string STR_END = "])";
+    }
 
-    class MainClass {
+    class LoadFolder
+    {
+        public IReadOnlyList<string> Folders = new List<string>();
 
-        private const int FIRST_INDEX = 1;
-        private const string STR_BEGIN = "(ID[";
-        private const string STR_END = "])";
+        private static IReadOnlyList<string> GetCommandLineArgs()
+        {
+            // コマンドライン引数を配列で取得する
+            string[] files = Environment.GetCommandLineArgs();
 
-        static void Main() {
-            var main = new MainClass();
+            var strs = new List<string>();
+            if (files.Length > AppConstData.FIRST_INDEX)
+            {
+                for (int i = AppConstData.FIRST_INDEX; i < files.Length; i++)
+                {
+                    strs.Add(files[i]);
+                }
+            }
+            return strs;
+        }
 
-            var folders = main.GetFolder();
-            if (!folders.Any()) {
+        public bool Execute()
+        {
+            var folders = GetCommandLineArgs();
+            if (!folders.Any())
+            {
                 Console.WriteLine("実行ファイルにフォルダをドロップしてください。");
                 Console.ReadKey();
-                return;
+                return false;
             }
 
-            var subFolders = Directory.GetDirectories(folders[0]);
-            var userIdDict = new Dictionary<string, int>(subFolders.Length);
+            Folders = Directory.GetDirectories(folders[0]);
+            return Folders.Any();
+        }
+    }
+
+    class CheckUserDuplicate
+    {
+        private IReadOnlyList<string> _folders;
+
+        public CheckUserDuplicate(IReadOnlyList<string> folders)
+        {
+            _folders = folders;
+        }
+
+        public void Execute()
+        {
+            var userIdDict = new Dictionary<string, int>(_folders.Count);
             var errorData = new List<string>();
 
-            foreach (var item in subFolders) {
-                if (!item.Contains(STR_BEGIN)) {
+            foreach (var item in _folders)
+            {
+                if (!item.Contains(AppConstData.STR_BEGIN))
+                {
                     errorData.Add(item);
                     continue;
                 }
 
-                var str = item.Substring(0, item.IndexOf(STR_BEGIN));
+                var str = item.Substring(0, item.IndexOf(AppConstData.STR_BEGIN));
                 str = item.Replace(str, string.Empty);
-                str = str.Replace(STR_BEGIN, string.Empty);
-                str = str.Replace(STR_END, string.Empty);
+                str = str.Replace(AppConstData.STR_BEGIN, string.Empty);
+                str = str.Replace(AppConstData.STR_END, string.Empty);
 
-                if (!userIdDict.ContainsKey(str)) {
+                if (!userIdDict.ContainsKey(str))
+                {
                     userIdDict.Add(str, 0);
                 }
                 userIdDict[str]++;
             }
 
-            foreach (var valuePair in userIdDict.OrderBy(o => o.Value)) {
+            foreach (var valuePair in userIdDict.OrderBy(o => o.Value))
+            {
                 Console.WriteLine($"{valuePair.Value}:{valuePair.Key}");
             }
 
-            foreach (var str in errorData) {
+            foreach (var str in errorData)
+            {
                 Console.WriteLine(str);
             }
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            var loadFIle = new LoadFolder();
+            if (!loadFIle.Execute())
+            {
+                return;
+            }
+
+            var checkUserDuplicate = new CheckUserDuplicate(loadFIle.Folders);
+            checkUserDuplicate.Execute();
 
             Console.ReadKey();
-        }
-
-        public IReadOnlyList<string> GetFolder() {
-            // コマンドライン引数を配列で取得する
-            string[] files = Environment.GetCommandLineArgs();
-
-            var strs = new List<string>();
-            if (files.Length > FIRST_INDEX) {
-                for (int i = FIRST_INDEX; i < files.Length; i++) {
-                    strs.Add(files[i]);
-                }
-            }
-            return strs;
         }
     }
 }
